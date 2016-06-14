0730a66dae03ef00ae3c35bbf036bd609272210c
Matt Milner <mmilner08@yahoo.com>
2016-06-14 17:37:02 -0500

    Add files via upload

---
+++ Calculator/Calculator/ViewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Matt Milner on 6/14/16.
+//  Copyright © 2016 Matt Milner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@property (nonatomic, weak) IBOutlet UITextField *firstNumber;
+@property (nonatomic, weak) IBOutlet UITextField *secondNumber;
+@property (nonatomic, weak) IBOutlet UILabel *result;
+
+
+@end
+
